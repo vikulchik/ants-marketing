9eed72efcb889e6465b16791024c23cb96ab74a1
vikulchik <kvvendetta@gmail.com>
2019-11-04 22:49:50 +0200

    Добавлена валидация.

---
+++ precache-manifest.a88bdcc422b6fa02f0bb09393d387d04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fe85239525eb244e546c9bcf5ad61f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b31274db704790c24d",
+    "url": "/static/css/main.00c35296.chunk.css"
+  },
+  {
+    "revision": "7ac57772082c34890811",
+    "url": "/static/js/2.cb236d92.chunk.js"
+  },
+  {
+    "revision": "09b31274db704790c24d",
+    "url": "/static/js/main.b76f9316.chunk.js"
+  },
+  {
+    "revision": "529d65e75bbb143dead8",
+    "url": "/static/js/runtime-main.2388f59f.js"
+  }
+]);
